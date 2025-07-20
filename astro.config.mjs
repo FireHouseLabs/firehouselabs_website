488d011e3fd65400cb2f01ea9d29d8c39e8b0730
jeremyw24 <jeremy@visionswest.com.au>
2025-07-20 22:02:09 +1000

    feat: add sitemap integration and enhance Google Analytics tracking for project interactions across various pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://firehouselabs.com.au',
   viewTransitions: true,
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress(), sitemap()],
 });
